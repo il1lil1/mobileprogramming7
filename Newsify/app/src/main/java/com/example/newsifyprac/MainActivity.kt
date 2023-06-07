3ab91247464c40554cccf72ed8f042a7d8ccc856
minhooooo <andy990401@gmail.com>
2023-06-07 11:48:46 +0900

    minoooooooo (+design)

--- Newsify/app/src/main/java/com/example/newsifyprac/MainActivity.kt
+++ Newsify/app/src/main/java/com/example/newsifyprac/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.newsifyprac
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.newsifyprac.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 val newsDataManager = NewsDataManager()
 var data_real = arrayListOf<NewsData>()
@@ -18,6 +20,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initLayout()
+
+
     }
 
     fun initLayout(){
@@ -25,32 +29,7 @@ class MainActivity : AppCompatActivity() {
         data_real = newsDataManager.getNews()
 
         myViewModel.setLiveData(0)
-        val fragment = supportFragmentManager.beginTransaction()
-        val newsFragment = NewsFragment()
-        fragment.replace(R.id.frameLayout, newsFragment)
-        fragment.commit()
 
-        binding.news.setOnClickListener {
-            val fragment = supportFragmentManager.beginTransaction()
-            fragment.addToBackStack(null)
-            val newsFragment = NewsFragment()
-            fragment.replace(R.id.frameLayout, newsFragment)
-            fragment.commit()
-        }
-        binding.mypage.setOnClickListener {
-            val fragment = supportFragmentManager.beginTransaction()
-            fragment.addToBackStack(null)
-            val mypageFragment = MypageFragment()
-            fragment.replace(R.id.frameLayout, mypageFragment)
-            fragment.commit()
-        }
-        binding.setting.setOnClickListener {
-            val fragment = supportFragmentManager.beginTransaction()
-            fragment.addToBackStack(null)
-            val settingFragment = SettingFragment()
-            fragment.replace(R.id.frameLayout, settingFragment)
-            fragment.commit()
-        }
         binding.home.setOnClickListener {
             val fragment = supportFragmentManager.beginTransaction()
             fragment.addToBackStack(null)
@@ -58,5 +37,32 @@ class MainActivity : AppCompatActivity() {
             fragment.replace(R.id.frameLayout, newsFragment)
             fragment.commit()
         }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_news -> {
+                    loadFragment(NewsFragment())
+                    true
+                }
+                R.id.navigation_mypage -> {
+                    loadFragment(MypageFragment())
+                    true
+                }
+                R.id.navigation_settings -> {
+                    loadFragment(SettingFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+        // To display initially selected fragment
+        loadFragment(NewsFragment())
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout, fragment)
+        transaction.commit()
     }
 }
